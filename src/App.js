ce17ccede7eca16c37bc6e07cba4cc1ebf8d04b6
Bruno Lima <brunolima@Brunos-iMac-2.local>
2020-01-29 17:11:53 -0300

    Well functional prototype

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import { initial, pizzas } from './pizzas.js'
+import './App.css'
 
 function App() {
+  let [loading, setLoading] = useState(false)
+  let [current, setCurrent] = useState(initial)
+  /**
+   * This funcion select's a random pizza from the "pizzas" stack
+   * and set the reactive variables to their new state
+   */
+  const randomize = () => {
+    // hurrp durr
+    setLoading(true)
+    // Creates a new stack of pizzas without the current selected
+    const haystack = [...pizzas]
+    haystack.splice(pizzas.indexOf(current), 1)
+    // A random index number to be the needle for the pizza hystack
+    const needle = Math.floor(Math.random() * haystack.length)
+    // Changes the current pizza to the new one as the animation continues
+    setTimeout(() => setCurrent(haystack[needle]), 750)
+    // Ends the rotation animation
+    setTimeout(() => setLoading(false), 1500)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="rdpizza">
+      <div className={`rdpizza__pizza ${(loading ? 'rdpizza__pizza--loading' : '')}`}>
+        <img alt="pizza" src={current.img} />
+      </div>
+      <h3>{current.name}</h3>
+      <button disabled={loading} onClick={randomize}>Randomize!</button>  
     </div>
   );
 }
---
+++ src/pizzas.js
@@ -0,0 +1,15 @@
+import initialPNG from './assets/initial.png'
+import piclesPNG from './assets/picles.png'
+import peperoniPNG from './assets/peperoni.png'
+import vegetarianPNG from './assets/vegetarian.png'
+
+export const initial = { 
+  name: 'Start Now!', 
+  img: initialPNG 
+}
+
+export const pizzas = [
+  { name: 'Picles', img: piclesPNG },
+  { name: 'Peperoni', img: peperoniPNG },
+  { name: 'Vegetarian', img: vegetarianPNG }
+]
